2cecbf6d9961d9183d3a35b6dbd437b6e5956d4a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-09 09:01:53 +0100

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,26 +1,22 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+        <div className="py-20 flex flex-col lg:flex-row items-center">
+          {/* Left side text */}
+          <h3 className="text-3xl md:text-4xl font-bold tracking-tight text-center lg:text-left mb-8 lg:mb-0 lg:pr-4 lg:w-1/2">
+            Statically Generated with Next.js
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+
+          {/* Right side button */}
+          <div className="flex justify-center lg:justify-start lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="https://trendingnews.globelynks.com"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors text-center"
             >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
+              Read Trending News
             </a>
           </div>
         </div>
