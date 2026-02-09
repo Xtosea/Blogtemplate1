cad6e6099320e3afbe53b1909eb05bb40ad07d7a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-09 08:47:23 +0100

    Update post-body.tsx

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -2,13 +2,14 @@ import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
   content: string;
+  className?: string; // optional for extra spacing
 };
 
-export function PostBody({ content }: Props) {
+export function PostBody({ content, className }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className={`max-w-2xl mx-auto ${className ?? ""}`}>
       <div
-        className={markdownStyles["markdown"]}
+        className={`${markdownStyles["markdown"]} prose prose-lg prose-slate`}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
